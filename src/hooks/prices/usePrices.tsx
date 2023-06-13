387841080656570ea75bd25d85c895fd5f3df61e
Robert Wach <wach.wachira7@gmail.com>
2023-06-13 22:31:28 +0300

    fixed pricelist hook

--- src/hooks/prices/usePrices.tsx
+++ src/hooks/prices/usePrices.tsx
@@ -4,19 +4,11 @@ import { apiUrl } from '../../config-global'
 export default function usePrices() {
     const url = `${apiUrl}/pricelist`
     const { error, data, mutate } = useSWR(url, { suspense: true })
-    try {
-        return {
-            loading: !error && !data,
-            error,
-            prices: data ? data.pricelist : [],
-            mutate,
-        }
-    } catch (err: any) {
-        return {
-            loading: false,
-            error: err?.error ?? err?.message,
-            prices: [],
-            mutate: false,
-        }
+
+    return {
+        loading: !error && !data,
+        error,
+        prices: data ? data.pricelist : [],
+        mutate,
     }
 }
